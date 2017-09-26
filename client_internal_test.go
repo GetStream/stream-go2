dbcc8780c6ccd398db80e6499266e4ab3f60830f
Federico Ruggi <f.ruggi@icloud.com>
2017-09-26 14:59:44 +0200

    moved test

--- client_internal_test.go
+++ client_internal_test.go
@@ -7,26 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_URLString(t *testing.T) {
-	testCases := []struct {
-		url      *apiURL
-		expected string
-	}{
-		{
-			url:      &apiURL{},
-			expected: "https://api.getstream.io/api/v1.0/",
-		},
-		{
-			url:      &apiURL{region: "eu-central", version: "2.0"},
-			expected: "https://eu-central-api.getstream.io/api/v2.0/",
-		},
-	}
-
-	for _, tc := range testCases {
-		assert.Equal(t, tc.expected, tc.url.String())
-	}
-}
-
 func Test_makeEndpoint(t *testing.T) {
 	testCases := []struct {
 		url      *apiURL
---
+++ url_internal_test.go
@@ -0,0 +1,27 @@
+package stream
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_URLString(t *testing.T) {
+	testCases := []struct {
+		url      *apiURL
+		expected string
+	}{
+		{
+			url:      &apiURL{},
+			expected: "https://api.getstream.io/api/v1.0/",
+		},
+		{
+			url:      &apiURL{region: "eu-central", version: "2.0"},
+			expected: "https://eu-central-api.getstream.io/api/v2.0/",
+		},
+	}
+
+	for _, tc := range testCases {
+		assert.Equal(t, tc.expected, tc.url.String())
+	}
+}
