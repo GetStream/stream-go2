4575024faf9e6663718cb62f75729156b80edecd
Federico Ruggi <f.ruggi@icloud.com>
2017-09-28 15:52:59 +0200

    better url generation

--- url.go
+++ url.go
@@ -8,13 +8,22 @@ type apiURL struct {
 }
 
 func (u *apiURL) String() string {
-	region := "api"
-	if u.region != "" {
-		region = u.region + "-" + region
+	if u.region == "localhost" {
+		return "http://localhost:8000/api/v1.0/"
 	}
-	version := "1.0"
+	return fmt.Sprintf("https://%s.getstream.io/api/v%s/", u.makeRegion(), u.makeVersion())
+}
+
+func (u *apiURL) makeVersion() string {
 	if u.version != "" {
-		version = u.version
+		return u.version
 	}
-	return fmt.Sprintf("https://%s.getstream.io/api/v%s/", region, version)
+	return "1.0"
+}
+
+func (u *apiURL) makeRegion() string {
+	if u.region != "" {
+		return fmt.Sprintf("%s-api", u.region)
+	}
+	return "api"
 }
