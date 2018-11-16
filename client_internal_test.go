12ba52a5f75496634886489a4f7043b1dee5e8ad
Tommaso Barbugli <tbarbugli@gmail.com>
2018-11-16 11:41:00 +0100

    fix internal test

--- client_internal_test.go
+++ client_internal_test.go
@@ -156,11 +156,11 @@ func Test_makeStreamError(t *testing.T) {
 			body:     strings.NewReader(`{"code":123, "detail":"test", "duration": "1m2s", "exception": "boom", "status_code": 456, "exception_fields": {"foo":["bar"]}}`),
 			expected: fmt.Errorf("test"),
 			apiErr: APIError{
-				Code:       123,
+				Code:       1,
 				Detail:     "test",
 				Duration:   Duration{time.Minute + time.Second*2},
 				Exception:  "boom",
-				StatusCode: 456,
+				StatusCode: 123,
 				ExceptionFields: map[string][]interface{}{
 					"foo": {"bar"},
 				},
