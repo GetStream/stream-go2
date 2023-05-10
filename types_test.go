171e42f2c8653937f3e097e00e56a52e7905278f
Jimmy Pettersson <jimmy.pettersson@getstream.io>
2023-05-10 15:54:51 +0200

    fix ratelimit marshal/unmarshal not in UTC and add test

--- types_test.go
+++ types_test.go
@@ -34,6 +34,62 @@ func TestTimeMarshalUnmarshalJSON(t *testing.T) {
 	err = json.Unmarshal(marshaled, &out)
 	assert.NoError(t, err)
 	assert.Equal(t, st, out)
+	zone, _ := out.Time.Zone()
+	assert.Equal(t, "UTC", zone)
+
+	// test in local timezone
+	now := stream.Time{Time: time.Now()}
+	b, err := json.Marshal(now)
+	require.NoError(t, err)
+	err = json.Unmarshal(b, &out)
+	require.NoError(t, err)
+	assert.Equal(t, now.Unix(), out.Unix())
+	zone, _ = out.Time.Zone()
+	assert.Equal(t, "UTC", zone)
+
+	// test in America/Los_Angeles timezone
+	la, err := time.LoadLocation("America/Los_Angeles")
+	require.NoError(t, err)
+	now = stream.Time{Time: time.Now().In(la)}
+	b, err = json.Marshal(now)
+	require.NoError(t, err)
+	err = json.Unmarshal(b, &out)
+	require.NoError(t, err)
+	assert.Equal(t, now.Unix(), out.Unix())
+	zone, _ = out.Time.Zone()
+	assert.Equal(t, "UTC", zone)
+
+	// test in UTC timezone
+	now = stream.Time{Time: time.Now().UTC()}
+	b, err = json.Marshal(now)
+	require.NoError(t, err)
+	err = json.Unmarshal(b, &out)
+	require.NoError(t, err)
+	assert.Equal(t, now.Unix(), out.Unix())
+	zone, _ = out.Time.Zone()
+	assert.Equal(t, "UTC", zone)
+
+	// test with unix timestamp
+	now = stream.Time{Time: time.Unix(1234, 0)}
+	b, err = json.Marshal(now)
+	require.NoError(t, err)
+	err = json.Unmarshal(b, &out)
+	require.NoError(t, err)
+	assert.Equal(t, now.Unix(), out.Unix())
+	assert.Equal(t, int64(1234), out.Unix())
+	zone, _ = out.Time.Zone()
+	assert.Equal(t, "UTC", zone)
+
+	// test with time.Date
+	d := time.Date(2023, time.May, 10, 15, 25, 52, 0, la)
+	now = stream.Time{Time: d}
+	b, err = json.Marshal(now)
+	require.NoError(t, err)
+	err = json.Unmarshal(b, &out)
+	require.NoError(t, err)
+	assert.Equal(t, d.Unix(), out.Unix())
+	zone, _ = out.Time.Zone()
+	assert.Equal(t, "UTC", zone)
 }
 
 func TestEnrichedActivityMarshal(t *testing.T) {
